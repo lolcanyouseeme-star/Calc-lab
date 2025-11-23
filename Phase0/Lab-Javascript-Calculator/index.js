2c061a0c5e08aa68eaf01bb1b3adca8984a99a97
Faizah <lolcanyouseeme@gmail.com>
2025-11-23 23:05:46 +0300

    Calculator

--- Phase0/Lab-Javascript-Calculator/index.js
+++ Phase0/Lab-Javascript-Calculator/index.js
@@ -1,15 +1,20 @@
+//ID because we have given it an ID on HTML
 let input = document.getElementById('inputBox');
+//This is to select all buttons
 let buttons = document.querySelectorAll('button');
 
+//Here is where we will store our result and display in this input
 let string = "";
+//To access the buttons and loops to attach click ability
 let arr = Array.from(buttons);
 arr.forEach(button => {
     button.addEventListener('click', (e) =>{
+        //Here when you press the button it is supposed to show the value
         if(e.target.innerHTML == '='){
             string = eval(string);
             input.value = string;
         }
-
+        
         else if(e.target.innerHTML == 'C'){
             string = ""
             input.value = string;
